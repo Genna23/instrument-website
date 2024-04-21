71a97eecf787bb8601d658a15a7db143012cf21a
Genna23 <mogilnikoff.gena@yandex.ru>
2024-04-21 18:20:09 +0300

    version 1.0

---
+++ src/data/navData.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        name: "Главная",
+        anchor: "#home"
+    },
+    {
+        name: "О нас",
+        anchor: "#about"
+    },
+    {
+        name: "Каталог",
+        anchor: "#catalog"
+    },
+    {
+        name: "Галерея",
+        anchor: "#gallery"
+    },
+    {
+        name: "FAQ",
+        anchor: "#faqs"
+    },
+    {
+        name: "Отзывы клиентов",
+        anchor: "#reviews"
+    },
+    {
+        name: "Контакты",
+        anchor: "#contacts"
+    },
+]
